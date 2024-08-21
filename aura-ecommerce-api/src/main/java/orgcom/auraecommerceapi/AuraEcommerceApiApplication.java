de6e767c7958dc147d3b65828d3273071bad25c4
ZakariaeNaaim <zakariaenaaim@gmail.com>
2024-08-21 18:16:14 +0100

    initialize backend

---
+++ aura-ecommerce-api/src/main/java/orgcom/auraecommerceapi/AuraEcommerceApiApplication.java
@@ -0,0 +1,13 @@
+package orgcom.auraecommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraEcommerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuraEcommerceApiApplication.class, args);
+    }
+
+}
---
+++ aura-ecommerce-api/src/test/java/orgcom/auraecommerceapi/AuraEcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package orgcom.auraecommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraEcommerceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
